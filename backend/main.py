1010b710d18884b86b74bb0ccf499d6de8273437
deekshithpatel10 <142518516+deekshithpatel10@users.noreply.github.com>
2024-06-14 20:33:25 +0530

    Minor fixes in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, jsonify  # type: ignore
+from flask import Flask, request, jsonify
 from io import BytesIO
 from PIL import Image
 import pytesseract
-from flask_cors import CORS # type: ignore
+from flask_cors import CORS 
 
 app = Flask(__name__)
 
@@ -14,8 +14,5 @@ def ocr():
    content = image_file.read()
    img = Image.open(BytesIO(content))
    text = pytesseract.image_to_string(img, lang='eng')
-   print(text)
    return jsonify({"text": text})
 
-if __name__ == "__main__":
-   app.run(debug=False)  # Set to False for production
